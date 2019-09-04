8020ddec3893ad95c6e17d0d1fd71ac405b5da38
Travis Agne <agnet16@students.bths201.org>
2019-09-04 14:23:13 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Travis Agne on 9/4/19.
+//  Copyright © 2019 Travis Agne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
